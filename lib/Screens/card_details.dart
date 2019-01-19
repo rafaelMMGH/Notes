b76200e7d4f679955881c048b8262902ad2724b5
Mac PC <rmartinez@igob.com.mx>
2019-01-18 19:37:03 -0600

    Add Features:
    * Create random Password

--- lib/Screens/card_details.dart
+++ lib/Screens/card_details.dart
@@ -26,6 +26,8 @@ class CardDetailState extends State<CardDetail> {
   Color color;
   int length = 4;
   int letters = 1;
+  bool checkBoxNumbers = true;
+  bool checkBoxSpecial = true;
 
   Note note;
 
@@ -47,7 +49,6 @@ class CardDetailState extends State<CardDetail> {
   @override
   void initState() {
     super.initState();
-    _showModalGeneratePassword(context);
   }
 
   void _checkValues(){
@@ -179,47 +180,10 @@ class CardDetailState extends State<CardDetail> {
         });
   }
 
-  _generatePassword(bool numbers,bool specials, int length, int letters) async {
-
-      var _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      var _numbers = "0123456789";
-      var _special = "!#%&'()*+,-./:;<=>?@[]^_`{|}~";
-      var _chars = "";
-      var _text ="";
-
-      final _random = new Random();
-
-      _chars += _letters.toLowerCase();
-
-      if (numbers)
-        _chars += _numbers;
-      if (specials)
-        _chars += _special;
-
-      int startIndex(int min, int max) => min + _random.nextInt(max - min);
-
-     // _chars += letters == 1 ? _letters : letters == 2 ? _letters.toLowerCase() : _letters + _letters.toLowerCase();
-
-
-
-      for (var i = 0; i < length; i++)
-        _text += _chars.substring(startIndex(1,_chars.length))[1];
-
-      debugPrint(_text);
-
-      descriptionController.text = _text.toString();
-
-
-      Navigator.of(context).pop();
-
-  }
-
   _showModalGeneratePassword(BuildContext context) async {
 
     final double width = MediaQuery.of(context).size.width;
     final double height = MediaQuery.of(context).size.height;
-    bool checkBoxNumbers = true;
-    bool checkBoxSpecial = true;
 
     await showModalBottomSheet<String>(
         context: context,
@@ -232,62 +196,7 @@ class CardDetailState extends State<CardDetail> {
                   borderRadius: new BorderRadius.only(
                       topLeft: const Radius.circular(15.0),
                       topRight:  const Radius.circular(15.0)),),
-                child: new Column(mainAxisSize: MainAxisSize.min,children: <Widget>[
-                  new GestureDetector(
-                    onLongPress: (){
-                      _fireAndThud();
-                    },
-                    child:
-                    new Image.asset("assets/password.jpg",alignment: Alignment.topCenter,repeat: ImageRepeat.noRepeat,width: width/2,height: height/6,),
-                  ),
-
-                  // new Image.network("https://cdn.dribbble.com/users/1748539/screenshots/4857111/dribbble_shot_hd.jpg",alignment: Alignment.topCenter,repeat: ImageRepeat.noRepeat,width: width/2,height: height/6,),
-
-                  Padding(
-                      padding: new EdgeInsets.all(0.0),
-                      child: new Column(
-                        children: <Widget>[
-                          new Text("Options",style: TextStyle(color: Color.fromRGBO(23, 23, 23, 1.0)),textAlign: TextAlign.left,),
-                          new SwitchListTile(
-                              value: checkBoxNumbers,
-                              title: new Text("Numbers"),
-                              secondary: const Icon(Icons.filter_9_plus),
-                              onChanged: (bool value) {
-                                setState(() {
-                                  checkBoxNumbers = value;
-                                });
-                              }
-                          ),
-                          new SwitchListTile(
-                              value: checkBoxSpecial,
-                              title: new Text("Special Characters"),
-                              secondary: const Icon(Icons.font_download),
-                              onChanged: (bool value) {
-                                setState(() {
-                                  checkBoxSpecial = value;
-                                });
-                              }
-                          ),
-                          new SingleChildScrollView(
-                            child: new Material(
-                              child: new MyDialogContent(length: length),
-                            ),
-                          ),
-
-                          new CupertinoButton(
-                              onPressed: (){
-                                setState(() {
-                                  _generatePassword(checkBoxNumbers,checkBoxSpecial,length,letters);
-                                });
-                              },
-                              child: new Text('Generate'))
-                        ],
-                      )
-
-
-                  ),
-
-                ],),
+                child: new MyBottomSheetDialog(length: length,),
               )
 
           );
@@ -407,7 +316,7 @@ class CardDetailState extends State<CardDetail> {
                                 fillColor: Colors.grey[150],
                                 filled: true,
                                 prefixIcon: IconButton(icon: Icon(_iconPassword),color: Colors.blue[700],onPressed: (){_toggle(); },),
-                                suffixIcon: IconButton(icon: Icon(Icons.threesixty),color: Colors.black,onPressed: (){ _showModalGeneratePassword(context); },),
+                                suffixIcon: IconButton(icon: Icon(Icons.threesixty),color: Colors.black,onPressed: (){ _showModalGeneratePassword(context); },),   //vxvbvcnbcbnjhghjgjgjgjghjhgjghjvxvbvcnbcbnjhghjgjgjgjghjhgjghjvxvbvcnbcbnjhghjgjgjgjghjhgjghjvxvbvcnbcbnjhghjgjgjgjghjhgjghj
                                 labelText: 'Password',
                                 labelStyle: textStyle,
                                 errorText: _passwordValidate ? 'Value Can\'t Be Empty' : null,
@@ -455,7 +364,7 @@ class _MyDialogContentState extends State<MyDialogContent> {
 
   _getContent(){
     if (widget.length == 4){
-      return     new Row(
+      return new Row(
         children: <Widget>[
           new Padding(padding: new EdgeInsets.symmetric(horizontal: 9)),
           new Icon(Icons.settings_ethernet,color: Colors.grey[600],),
@@ -502,3 +411,236 @@ class _MyDialogContentState extends State<MyDialogContent> {
   }
 }
 
+class GeneratePassword extends StatefulWidget{
+  GeneratePassword({
+    Key key,
+    this.checkBoxNumbers,
+    this.checkBoxSpecial,
+    this.length,
+    this.letters,
+  }): super(key:key);
+
+  final bool checkBoxNumbers;
+  final bool checkBoxSpecial;
+  final int length;
+  final int letters;
+
+  @override
+  _GeneratePasswordState createState() => new _GeneratePasswordState();
+
+}
+
+class _GeneratePasswordState extends State<GeneratePassword> {
+  bool _checkBoxNumbers;
+  bool _checkBoxSpecial;
+  int _letters;
+  int _selectedIndex = 4;
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  _getContent(){
+    if (widget.length == 4){
+      return new CupertinoButton(
+          onPressed: (){
+            setState(() {
+              //_generatePassword(checkBoxNumbers,checkBoxSpecial,length,letters);
+            });
+          },
+          child: new Text('Generate'));
+    }
+
+    new Row(
+      children: <Widget>[
+        new Icon(Icons.settings_ethernet),
+        new Text('Length: $_selectedIndex'),
+        new Slider(
+            value: _selectedIndex.toDouble(),
+            min: 0.0,
+            max: 100.0,
+            activeColor: Colors.blue,
+            onChanged: (double value) {
+              setState(() {
+                _selectedIndex = value.round();
+              });
+            }),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _getContent();
+  }
+
+  _generatePassword(bool numbers,bool specials, int length, int letters) async {
+
+    var _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var _numbers = "0123456789";
+    var _special = "!#%&'()*+,-./:;<=>?@[]^_`{|}~";
+    var _chars = "";
+    var _text ="";
+
+    final _random = new Random();
+
+    _chars += _letters.toLowerCase();
+
+    if (numbers)
+      _chars += _numbers;
+    if (specials)
+      _chars += _special;
+
+    int startIndex(int min, int max) => min + _random.nextInt(max - min);
+
+    // _chars += letters == 1 ? _letters : letters == 2 ? _letters.toLowerCase() : _letters + _letters.toLowerCase();
+
+
+
+    for (var i = 0; i < length; i++)
+      _text += _chars.substring(startIndex(1,_chars.length))[1];
+
+    debugPrint(_text);
+    debugPrint(length.toString());
+
+    //  passwordController.text = _text.toString();
+
+
+    Navigator.of(context).pop();
+
+  }
+}
+
+class MyBottomSheetDialog extends StatefulWidget{
+  MyBottomSheetDialog({
+    Key key,
+    this.length,
+    this.letters,
+    this.number,
+    this.special
+}):super(key:key);
+
+  final int length;
+  final int letters;
+  final bool number;
+  final bool special;
+
+  @override
+  _MyBottomSheetDialogState createState() => new _MyBottomSheetDialogState();
+
+
+
+}
+
+class _MyBottomSheetDialogState extends State<MyBottomSheetDialog>{
+  int _selectLength = 4;
+  int _selectLetters = 1;
+  bool _checkBoxNumbers = true;
+  bool _checkBoxSpecial = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  void _fireAndThud(){
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(9.0)
+              ),
+              title: new Text('About',textAlign: TextAlign.center,),
+              content: new Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Text(' The day after you stole my heart,\nEverything I touched told me it would be better shared with you', textAlign: TextAlign.justify,),
+                  new Divider(),
+                ],
+              )
+          );
+        });
+  }
+
+
+  _getContent(BuildContext context){
+
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+        new GestureDetector(
+          onLongPress: (){
+            _fireAndThud();
+          },
+          child:
+          new Image.asset("assets/password.jpg",alignment: Alignment.topCenter,repeat: ImageRepeat.noRepeat,width: width/2,height: height/6,),
+        ),
+
+        // new Image.network("https://cdn.dribbble.com/users/1748539/screenshots/4857111/dribbble_shot_hd.jpg",alignment: Alignment.topCenter,repeat: ImageRepeat.noRepeat,width: width/2,height: height/6,),
+
+        Padding(
+            padding: new EdgeInsets.all(0.0),
+            child: new Column(
+              children: <Widget>[
+                //new Text("Options",style: TextStyle(color: Color.fromRGBO(23, 23, 23, 1.0)),textAlign: TextAlign.left,),
+                new SwitchListTile(
+                    value: _checkBoxNumbers,
+                    title: new Text("Numbers"),
+                    secondary: const Icon(Icons.filter_9_plus),
+                    onChanged: (bool value) {
+                      setState(() {
+                        _checkBoxNumbers = value;
+                      });
+                    }
+                ),
+                new SwitchListTile(
+                    value: _checkBoxSpecial,
+                    title: new Text("Special Characters"),
+                    secondary: const Icon(Icons.font_download),
+                    onChanged: (bool value) {
+                      setState(() {
+                        _checkBoxSpecial = value;
+                      });
+                    }
+                ),
+                new SingleChildScrollView(
+                  child: new Material(
+                    child: new MyDialogContent(length: _selectLength),
+                  ),
+                ),
+                /*new SingleChildScrollView(
+                  child: new Material(
+                    child: new GeneratePassword(checkBoxNumbers: checkBoxNumbers,checkBoxSpecial: checkBoxSpecial,length: length,letters: letters),
+                  ),
+                ),*/
+                  new CupertinoButton(
+                  onPressed: (){
+                    setState(() {
+
+                    //_generatePassword(checkBoxNumbers,checkBoxSpecial,length,letters);
+                    });
+                  },
+                  child: new Text('Generate'))
+
+              ],
+            )
+
+        ),
+      ],
+      );
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return _getContent(context);
+  }
+}
